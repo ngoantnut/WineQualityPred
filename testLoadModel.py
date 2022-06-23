98465bef330aeccccb22ae6294ab5b08903a9568
ngoantnut <73264521+ngoantnut@users.noreply.github.com>
2022-06-23 20:53:11 +0700

    Add files via upload

--- testLoadModel.py
+++ testLoadModel.py
@@ -11,17 +11,17 @@ st.title('Dự đoán chất lượng rượu vang: ')
 col1, col2, col3 =st.columns(3)
 
 # Id= np.str(col1.text_input("Id"))
-fixed_acidity= np.str(col1.text_input('fixed acidity'))
-volatile_acidity= np.str(col2.text_input('volatile acidity'))
-citric_acid= np.str(col3.text_input('citric acid'))
-residual_sugar= np.str(col1.text_input('residual sugar'))
-chlorides= np.str(col2.text_input('chlorides'))
-free_sulfur_dioxide= np.str(col3.text_input('free sulfur dioxide'))
-total_sulfur_dioxide= np.str(col1.text_input('total sulfur dioxide'))
+fixed_acidity= np.str(col1.slider('fixed acidity', 0.0, 20.0, 5.0))
+volatile_acidity= np.str(col2.slider('volatile acidity (g/L)', 0.00, 2.00, 0.55))
+citric_acid= np.str(col3.slider('citric acid (g/L)', 0.00, 1.00, 0.55))
+residual_sugar= np.str(col1.slider('residual sugar(g/L)', 0.00, 25.00, 10.00))
+chlorides= np.str(col2.slider('chlorides', 0.000,0.001, 0.2, 0.001, format="%f"))
+free_sulfur_dioxide= np.str(col3.slider('free sulfur dioxide'  ))
+total_sulfur_dioxide= np.str(col1.slider('total sulfur dioxide', 0, 250, 100))
 density= np.str(col2.text_input('density'))
-pH= np.str(col3.text_input('pH'))
-sulphates= np.str(col1.text_input('sulphates'))
-alcohol= np.str(col2.text_input('alcohol'))
+pH= np.str(col3.slider('pH', 0.00, 5.00, 3.00))
+sulphates= np.str(col1.slider('sulphates', 0.00, 2.00, 1.00))
+alcohol= np.str(col2.slider('alcohol', 0.0, 25.0, 10.0 ))
 
 features_num = ['fixed acidity','volatile acidity', 'citric acid',
                 'residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide',
@@ -34,10 +34,8 @@ sample_df=pd.DataFrame([sample], columns=features_num)
 model = pickle.load(open("finalized_model.sav","rb"))
 if result:
     st.write('Chất lượng của rượu vang là:  ' , str(model.predict(sample_df)))
-if result >= 8:
-    st.write('Rượu chất lượng tốt!')
-else:
-    st.write('Rượu tệ!')
+
+
 st.info('Bài tập lớn Trí tuệ nhân tạo - Trần Ngoan')
 st.info('Mssv: K185480106014 ')
 st.info(' Email:tvngoan343@gmail.com')
